1376e11c1e55c3e696a81f2b711467a04bee491c
Sepiolx64 <62660052+Sepiolx64@users.noreply.github.com>
2023-04-13 21:37:39 +0200

    Update salon.sh

--- salon.sh
+++ salon.sh
@@ -38,9 +38,15 @@ if [[ ! $SERVICE_ID_SELECTED =~ ^[0-9]+$ ]]
             echo -e "\nPlease enter your name:\n"
             read CUSTOMER_NAME
             INSERT_NEW_CUSTOMER=$($PSQL "INSERT INTO customers(phone, name) VALUES('$CUSTOMER_PHONE', '$CUSTOMER_NAME')")
-            echo $INSERT_NEW_CUSTOMER
           fi
-          
+          echo -e "\nPlease enter a time:"
+          read SERVICE_TIME
+          CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+          INSERT_APPOINTMENT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES('$CUSTOMER_ID', '$SERVICE_ID_SELECTED', '$SERVICE_TIME')")
+          if [[ $INSERT_APPOINTMENT ]]
+            then
+              echo -e "\nI have put you down for a$NAME$($PSQL "SELECT name FROM services WHERE service_id='$SERVICE_ID_SELECTED'") at $SERVICE_TIME, $CUSTOMER_NAME."
+            fi
       fi
   fi
 }
